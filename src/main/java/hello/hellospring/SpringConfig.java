4164dffc9e79c5852ad1caca5456cd47186f7888
myengbaePark <pmb.dev3@gmail.com>
2024-02-15 15:01:32 +0900

    회원가입, 회원목록 기능

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,22 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    // 빌드 과정에서 Spring이 Configuration에있는 Bean을 등록함
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
